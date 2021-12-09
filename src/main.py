14d2b10e55dd38699d9d1fc415f40f468b85c5cf
Ruslan Galimov <rg@warp.is>
2021-12-09 07:26:52 +0000

    ! wip

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-import os
-import dotenv
-from uploader import Uploader
 import json
+import os
+import time
+
+import dotenv
+
+from uploader import Uploader
+
 
 def main():
     # Initialize env variables
@@ -14,31 +18,30 @@ def main():
     uploader.connect_metamask(seed_phrase, password)
 
     # Connect to the specified network - ENTER THE APPROPRIATE NETWORK
-    NETWORK_RPC = "https://rpc-mumbai.maticvigil.com/"
-    CHAIN_ID = 80001
-    uploader.set_network(NETWORK_RPC, CHAIN_ID) # Custom network to add to Metamask
+    NETWORK_RPC = "https://polygon-mainnet.g.alchemy.com/v2/nV3XqnDJF7y7bmXTQhrJB13lcK1aAAWF"
+    CHAIN_ID = 137
+    uploader.set_network(NETWORK_RPC, CHAIN_ID)  # Custom network to add to Metamask
     uploader.open_metamask()
-    # uploader.set_network("", 0, 1) # Use a default network provided by Metamask
 
     # Connect to OpenSea
-    uploader.connect_opensea(test=True)
-    COLLECTION_URL = "https://testnets.opensea.io/collection/big-test-4"
+    uploader.connect_opensea(False)
+    COLLECTION_URL = "https://opensea.io/collection/mewmodernsimplecollection"
     uploader.set_collection_url(COLLECTION_URL)
 
     # Upload NFT data in 'metadata.json' to OpenSea - MODIFY THE UPLOAD FUNCTION AND THE METADATA TO CONTAIN ANY ADDITIONAL METADATA
     metadata = json.load(open(os.path.join(os.getcwd(), "data", "metadata.json")))
     first_upload = True
     for i, data in enumerate(metadata):
-        try:
-            uploader.upload(os.path.join(os.getcwd(), "data", "assets", data["asset"]), data["name"])
-            if first_upload:
-                uploader.sign_transaction()
-                first_upload = False 
-        except Exception as e:
-            print(f"Failed to upload NFT {i} '{data['name']}' for reason '{e}'.")
-
+        uploader.upload(data)
+        if first_upload:
+            uploader.sign_transaction()
+            first_upload = False
+        time.sleep(2)
+        uploader.sell()
+        time.sleep(1)
     # Close
     uploader.close()
+
 
 # Run main if this file is run directly
 if __name__ == "__main__":
--- src/uploader.py
+++ src/uploader.py
@@ -1,6 +1,10 @@
-from selenium import webdriver
-from time import sleep
 import os
+import time
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class Uploader:
     def __init__(self):
@@ -15,67 +19,93 @@ class Uploader:
         # Initialize the driver
         opt = webdriver.ChromeOptions()
         opt.add_extension(extension=ext_path)
-        opt.add_argument('--log-level=3')
+        opt.add_argument("--log-level=3")
         self.__driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=opt)
 
         # Close the metamask popup and navigate back to the correct window
-        sleep(2)
+        sleep(4)
         self.__driver.switch_to.window(self.__driver.window_handles[0])
         self.__driver.close()
         self.__driver.switch_to.window(self.__driver.window_handles[0])
 
-
     def connect_metamask(self, seed_phrase: str, password: str):
-        '''
+        """
         Connect to Metamask
-        '''
+        """
 
         # Navigate to metamask screen
         self.__driver.get(f"{self.__METAMASK_URL}#initialize/welcome")
         sleep(1)
 
         # Skip through wallet setup screen
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div/div/button').click()
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div/div[2]/div/div[2]/div[1]/button').click()
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div/div/div[5]/div[1]/footer/button[1]').click()
+        self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div/div/button').click()
+        self.__driver.find_element(
+            By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div/div[2]/div/div[2]/div[1]/button'
+        ).click()
+        self.__driver.find_element(
+            By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div/div/div[5]/div[1]/footer/button[1]'
+        ).click()
         sleep(0.5)
 
         # Enter wallet seed phrase and password
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div/form/div[4]/div[1]/div/input').send_keys(seed_phrase)
-        self.__driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
-        self.__driver.find_element_by_xpath('//*[@id="confirm-password"]').send_keys(password)
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div/form/div[7]/div').click()
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div/form/button').click()
+        self.__driver.find_element(
+            By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div/form/div[4]/div[1]/div/input'
+        ).send_keys(seed_phrase)
+        self.__driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
+        self.__driver.find_element(By.XPATH, '//*[@id="confirm-password"]').send_keys(password)
+        self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div/form/div[7]/div').click()
+        self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div/form/button').click()
         sleep(2)
 
     def set_network(self, rpc_url: str, chain_id: int, preconfigured_network: int = None):
-        '''
+        """
         Sets the specified network to Metamask and selects it. Also adds it if it is not a default network.
-        '''
+        """
 
         # Go to the networks tab
         self.__driver.get("data:")
+        sleep(1)
         self.__driver.get(f"{self.__METAMASK_URL}#settings/networks")
-        sleep(1.5)
+        sleep(2)
 
         # Choose one of the preconfigured networks if specified
-        if preconfigured_network == None:
-            self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[1]/div/button').click()
-            self.__driver.find_element_by_xpath('//*[@id="network-name"]').send_keys("Network")
-            self.__driver.find_element_by_xpath('//*[@id="rpc-url"]').send_keys(rpc_url)
-            self.__driver.find_element_by_xpath('//*[@id="chainId"]').send_keys(chain_id)
-            self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[2]/div[2]/div[7]/button[2]').click()
+        if preconfigured_network is None:
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[1]/div/button'
+            ).click()
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[1]/label/input'
+            ).send_keys("Network")
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/label/input'
+            ).send_keys(rpc_url)
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[3]/label/input'
+            ).send_keys(chain_id)
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div/div[2]/div/div[3]/button[2]'
+            ).click()
             preconfigured_network = 7
+        else:
+            # Select the network
+            self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[1]/div/div[2]/div[1]/div').click()
+            self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[1]/div[3]/span/a').click()
 
-        # Select the network
-        self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[1]/div/div[2]/div[1]/div').click()
-        self.__driver.find_element_by_xpath(f'//*[@id="app-content"]/div/div[2]/div/li[{preconfigured_network}]').click()
+            self.__driver.find_element(
+                By.XPATH,
+                '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div[2]/div[7]/div[2]/div/div/div[1]/div[2]/div',
+            ).click()
+            self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[1]/div/div[2]/div[1]/div').click()
+            sleep(1)
+            self.__driver.find_element(
+                By.XPATH, f'//*[@id="app-content"]/div/div[2]/div/div[2]/div/li[{preconfigured_network}]'
+            ).click()
         sleep(2)
 
     def open_metamask(self):
-        '''
+        """
         Open Metamask in new window
-        '''
+        """
 
         # Open the extension in a new tab and switch back
         self.__driver.execute_script("window.open('');")
@@ -84,9 +114,9 @@ class Uploader:
         self.__driver.switch_to.window(self.__driver.window_handles[0])
 
     def __metamask_execute(self, fn):
-        '''
+        """
         Execute an operation within Metamask and then switch back
-        '''
+        """
 
         self.__driver.switch_to.window(self.__driver.window_handles[1])
         self.__driver.refresh()
@@ -96,52 +126,95 @@ class Uploader:
         self.__driver.switch_to.window(self.__driver.window_handles[0])
 
     def connect_opensea(self, test: bool):
-        '''
+        """
         Connect OpenSea with Metamask
-        '''
+        """
 
         self.__driver.get("https://testnets.opensea.io/login" if test else "https://opensea.io/login")
         sleep(2)
-        self.__driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/div/div[2]/ul/li[1]/button').click()
+        self.__driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/main/div/div/div/div[2]/ul/li[1]/button').click()
         sleep(1)
+
         def connect():
-            self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div[2]/div[4]/div[2]/button[2]').click()
-            self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[4]/div[2]/button[2]'
+            ).click()
+            self.__driver.find_element(
+                By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]'
+            ).click()
             sleep(2)
+
         self.__metamask_execute(connect)
 
     def sign_transaction(self):
         def sign():
-            self.__driver.find_element_by_xpath('//*[@id="app-content"]/div/div[2]/div/div[3]/button[2]').click()
+            try:
+                self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[3]/button[2]').click()
+            except Exception:
+                self.__driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/button[2]').click()
             sleep(1)
+
         self.__metamask_execute(sign)
 
     def set_collection_url(self, collection_url: str):
-        '''
+        """
         Sets the OpenSea collection URL to upload to
-        '''
+        """
 
         self.__collection_url = collection_url
 
-    def upload(self, asset_path: str, name: str):
-        '''
+    def upload(self, data):
+        """
         Upload a single NFT to OpenSea.
-        '''
+        """
 
         # Add an item to the collection
         self.__driver.get(f"{self.__collection_url}/assets/create")
         sleep(1)
 
         # Input the data
-        self.__driver.find_element_by_xpath('//*[@id="media"]').send_keys(asset_path)
-        self.__driver.find_element_by_xpath('//*[@id="name"]').send_keys(name)
+        self.__driver.find_element(By.XPATH, '//*[@id="media"]').send_keys(data["image"])
+        self.__driver.find_element(By.XPATH, '//*[@id="name"]').send_keys(data["name"])
+        self.__driver.find_element(
+            By.XPATH, '//*[@id="main"]/div/div/section/div/form/section/div[1]/div/div[2]/button'
+        ).click()
+        time.sleep(0.5)
+        for i, attribute in enumerate(data["attributes"], start=1):
+            self.__driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/section/button").click()
+            self.__driver.find_element(
+                By.XPATH, f"/html/body/div[2]/div/div/div/section/table/tbody/tr[{i}]/td[1]/div/div/input"
+            ).send_keys(attribute["trait_type"])
+
+            self.__driver.find_element(
+                By.XPATH, f"/html/body/div[2]/div/div/div/section/table/tbody/tr[{i}]/td[2]/div/div/input"
+            ).send_keys(attribute["value"])
+        self.__driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/footer/button").click()
+        time.sleep(0.5)
 
         # =================================
         # Add your other NFT metadata here
         # =================================
 
-        self.__driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/section/div[2]/form/div[9]/div[1]/span/button').click()
+        self.__driver.find_element(
+            By.XPATH, '//*[@id="main"]/div/div/section/div[2]/form/div[9]/div[1]/span/button'
+        ).click()
         sleep(2)
+
+    def sell(self):
+        self.__driver.find_element(By.XPATH, "/html/body/div[5]/div/div/div/div[2]/button/i").click()
+        time.sleep(1)
+        self.__driver.find_element(By.XPATH, '//*[@id="main"]/div/div/div[1]/div/span[2]/a').click()
+        time.sleep(2)
+        self.__driver.find_element(
+            By.XPATH, '//*[@id="main"]/div/div/div[2]/div/div[1]/div/form/div[1]/div/div[2]/div/div/div[2]/input'
+        ).send_keys(0.042)
+        self.__driver.find_element(By.XPATH, '//*[@id="main"]/div/div/div[2]/div/div[1]/div/form/div[5]/button').click()
+        time.sleep(2)
+        self.__driver.find_element(
+            By.XPATH, "/html/body/div[4]/div/div/div/section/div/div/section/div/div/div/div/div/div/div/button"
+        ).click()
+        time.sleep(1)
+        self.sign_transaction()
 
     def close(self):
         for window_handle in self.__driver.window_handles:
